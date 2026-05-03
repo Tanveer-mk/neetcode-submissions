0ff6b2602b437b5f8e82b6e815a1527c007dc936
Mohd Khaja Tanveeruddin <121084130+Tanveer-mk@users.noreply.github.com>
2026-05-03 19:04:35 +0530

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.java
@@ -0,0 +1,55 @@
+class Solution {
+    public void merge(int[] arr, int low, int mid, int high){
+        int n1 = mid-low+1;
+        int n2 = high-mid;
+
+        int[] lArr = new int[n1];
+        int[] rArr = new int[n2];
+        
+        for(int x = 0; x<n1; x++){
+            lArr[x] = arr[low+x];
+        }
+        for(int x = 0; x<n2; x++){
+            rArr[x] = arr[mid+1+x];
+        }
+        
+        int i = 0;
+        int j = 0;
+        int k = low;
+        
+        while(i < n1 && j < n2){
+            if(lArr[i] <= rArr[j]){
+                arr[k] = lArr[i];
+                i++;
+                k++;
+            }else if(rArr[j] <= lArr[i]){
+                arr[k] = rArr[j];
+                j++;
+                k++;
+            }
+        }
+        while(i < n1){
+            arr[k] = lArr[i];
+            i++;
+            k++;
+        }
+        while(j < n2){
+            arr[k] = rArr[j];
+            j++;
+            k++;
+        }
+    }
+
+    public void mergeSort(int[] arr, int low, int high){
+        if(low < high){
+            int mid = (low + high) / 2;
+            mergeSort(arr, low, mid);
+            mergeSort(arr, mid + 1, high);
+            merge(arr, low, mid, high);
+        }
+    }
+    public int[] sortArray(int[] nums) {
+        mergeSort(nums, 0, nums.length-1);
+        return nums;
+    }
+}
