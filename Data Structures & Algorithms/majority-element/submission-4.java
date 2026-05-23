81720a5b50ba154424cb76f9d38c9932f02ef312
Mohd Khaja Tanveeruddin <121084130+Tanveer-mk@users.noreply.github.com>
2026-05-23 17:22:58 +0530

    Add: majority-element - submission-4

---
+++ Data Structures & Algorithms/majority-element/submission-4.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int max = nums[0];
+        for(int i = 0; i<nums.length; i++){
+            if(nums[i] != max && count == 0){
+                max = nums[i];
+                count++;
+            }else if(nums[i] == max) count++;
+            else count--;
+        }
+        return max;
+    }
+}
